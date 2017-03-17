4677719a7671c6bd6d9ad41f4f90506999414068
Jacob J <jjohnson@atlasrfid.com>
2017-03-17 16:10:32 -0500

    moved authorization url to saved posts service

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SavedPostsService } from '../saved-posts/saved-posts.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent {
 
-  // Ran into some real trouble with the spacing on this url!
-  protected authorizationUrl = `https://www.reddit.com/api/v1/authorize?client_id=upw3i_YafZpoXw&response_type=code&state=string_of_random
-  &redirect_uri=http://localhost:4200/saved-posts&duration=temporary&scope=history identity`;
+  protected authorizationUrl = this._savedPostsService.savedPostsAuthorizationUrl;
+
+  constructor(private _savedPostsService: SavedPostsService) { }
 }
--- src/app/saved-posts/saved-posts.service.ts
+++ src/app/saved-posts/saved-posts.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SavedPostsService {
 
-  constructor() { }
-
+  // Ran into some real trouble with the spacing on this multi-line url!
+  public savedPostsAuthorizationUrl = `https://www.reddit.com/api/v1/authorize?client_id=upw3i_YafZpoXw&response_type=code` +
+  `&state=string_of_random&redirect_uri=http://localhost:4200/saved-posts&duration=temporary&scope=history`;
 }
