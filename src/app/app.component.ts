51324c488263619c4fd41c8f2c65527bbff8b6ea
Jacob J <jjohnson@atlasrfid.com>
2017-03-17 08:58:33 -0500

    refactored components a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Retainer';
-
-  // Ran into some real trouble with the spacing on this url!
-  protected authorizationUrl = `https://www.reddit.com/api/v1/authorize?client_id=upw3i_YafZpoXw&response_type=code&state=string_of_random
-  &redirect_uri=http://localhost:4200/posts&duration=temporary&scope=history identity`;
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,11 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  // Ran into some real trouble with the spacing on this url!
+  protected authorizationUrl = `https://www.reddit.com/api/v1/authorize?client_id=upw3i_YafZpoXw&response_type=code&state=string_of_random
+  &redirect_uri=http://localhost:4200/saved-posts&duration=temporary&scope=history identity`;
 }
