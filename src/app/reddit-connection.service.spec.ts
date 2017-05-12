933ee778bbb33d558444b2f2973eea20c8cd7f6c
jacob <gnrlalias@fastmail.us>
2017-05-11 21:41:42 -0500

    Add get authorization token with code

--- src/app/reddit-connection.service.spec.ts
+++ src/app/reddit-connection.service.spec.ts
@@ -3,6 +3,9 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { RedditConnectionService } from './reddit-connection.service';
 import { RandomServiceMockBuilder } from 'test/mock-builders/random-service-mock-builder';
+import { Http } from '@angular/http';
+import { RandomService } from 'app/random.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('Saved Posts Service', () => {
     beforeEach(() => {
@@ -11,25 +14,40 @@ describe('Saved Posts Service', () => {
         });
     });
 
-    it('should use a random string for the state in the url', () => {
-       const expectedStateString = Any.stateString(10);
+    function createService(
+        randomServiceMock: RandomService = new RandomServiceMockBuilder().build(),
+        httpMock: Http = jasmine.createSpyObj('Http', ['get'])): RedditConnectionService {
+        const service = new RedditConnectionService(randomServiceMock, httpMock);
+        return service;
+    }
 
-        const randomServiceMock = new RandomServiceMockBuilder()
-            .withGeneratedState(expectedStateString)
-            .build();
+    describe('Get Reddit Authorization Url', () => {
+        it('should use a random string for the state in the url', () => {
+            const expectedStateString = Any.stateString(10);
 
-        const service = new RedditConnectionService(randomServiceMock);
+            const randomServiceMock = new RandomServiceMockBuilder()
+                .withGeneratedState(expectedStateString)
+                .build();
 
-        expect(service.getRedditAuthorizationUrl()).toContain(expectedStateString);
+            const service = createService(randomServiceMock);
+
+            expect(service.getRedditAuthorizationUrl()).toContain(expectedStateString);
+        });
     });
 
-    // Continue here...
-    fit('should get a reddit authorization token with the url code', () => {
-        const randomServiceMock = new RandomServiceMockBuilder()
-            .build();
-        
-        const service = new RedditConnectionService(randomServiceMock);
+    describe('Get Authorization Token With Code', () => {
+        fit('should get a reddit authorization token with the url code', () => {
+            const httpMock = jasmine.createSpyObj('Http', ['post']);
+            httpMock.post.and.returnValue(Observable.of(/**/));
+            const service = createService(undefined, httpMock);
+            const expectedCode = Any.alphaNumericString(5);
 
+            const body = `grant_type=authorization_code&code=${expectedCode}` +
+                '&redirect_uri=http://localhost:4200/saved-posts';
 
+            service.getAuthorizationTokenWithCode(expectedCode);
+
+            expect(httpMock.post).toHaveBeenCalledWith('https://www.reddit.com/api/v1/access_token', body);
+        });
     });
 });
--- src/app/reddit-connection.service.ts
+++ src/app/reddit-connection.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { RandomService } from './random.service';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class RedditConnectionService {
 
     private _state: string;
+    private _token: string;
+
     public get state(): string {
         return this._state;
     }
 
-    public constructor(private _randomService: RandomService) {
+    public get token(): string {
+        return this._token;
+    }
+
+    public constructor(private _randomService: RandomService, private _http: Http) {
         this._state = this._randomService.generateStateString(20);
      }
 
@@ -20,7 +27,14 @@ export class RedditConnectionService {
             `&state=${this._state}&redirect_uri=http://localhost:4200/saved-posts&duration=temporary&scope=history`;
     }
 
-    public getAuthorizationTokenWithCode(code: string) {
-        //
+    public getAuthorizationTokenWithCode(code: string): void {
+        const body = `grant_type=authorization_code&code=${code}` +
+        '&redirect_uri=http://localhost:4200/saved-posts';
+        this._http.post('https://www.reddit.com/api/v1/access_token', body)
+            .subscribe(token => this.mapToken(token.json()), error => console.log(error));
+    }
+
+    private mapToken(token: string) {
+        this._token = token;
     }
 }
