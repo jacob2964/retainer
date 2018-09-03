1cd53e4700fccf09f2691059e39a1e2e9f31e339
jacob <gnrlalias@fastmail.us>
2018-09-03 05:36:02 -0500

    Change redirect url back to local.

--- src/app/retainer-configuration.ts
+++ src/app/retainer-configuration.ts
@@ -1,5 +1,5 @@
 export class RetainerConfig {
-    public static redirectUrl = 'http://jacob2964.github.io/retainer/';
+    public static redirectUrl = 'http://localhost:4200/';
     public static redditBaseUrl = 'https://www.reddit.com/';
     public static redditOauthUrl = 'https://oauth.reddit.com/';
 }
